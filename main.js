ad84302e4ccde4c8367ae39806be867db04003ff
idanaaber <kevin.randmaa@khk.ee>
2018-10-10 12:26:40 +0300

    Lisatud naelade ja untside kalkuleerimisvoimalus.

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 //DOM ELEMENTS
 let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
-
+let poundOutput = document.getElementById('poundOutput');
+let ozOutput = document.getElementById('ozOutput');
 //LISTENING TO EVENTS
 kgInput.addEventListener('input', event => {
 	let kg = event.target.value;
 	
 	gramOutput.innerHTML = `${kg * 1000} g`;
+	poundOutput.innerHTML = `${kg * 2.20462262} lb`;
+	ozOutput.innerHTML = `${kg * 35.2739619} oz`;
 });	
